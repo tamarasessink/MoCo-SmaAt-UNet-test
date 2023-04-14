ebdb0ad15c3ca5b2ed866fe172ee374838c62ef1
Tamara Sessink <t.r.sessink@students.uu.nl>
2023-04-14 15:39:49 +0200

    update project

--- models/SmaAt_Unet_pre_training.py
+++ models/SmaAt_Unet_pre_training.py
@@ -1,4 +1,6 @@
-from torch import nn
+import torch.nn as nn
+from torch import Tensor
+
 from models.unet_parts import OutConv
 from models.unet_parts_depthwise_separable import DoubleConvDS, UpDS, DownDS
 from models.layers import CBAM
@@ -7,17 +9,19 @@ from models.layers import CBAM
 # TODO: add copyright
 
 class SmaAt_UNet_pre(nn.Module):
-    def __init__(self, num_classes: 12):
+    def __init__(self) -> None:
         super(SmaAt_UNet_pre, self).__init__()
-        self.num_channels = 3
+        self.n_channels = 3
         self.kernels_per_layer = 2
         self.bilinear = True
         self.reduction_ratio = 16
+        self.num_classes = 128
 
         self.inc = DoubleConvDS(self.n_channels, 64, kernels_per_layer=self.kernels_per_layer)
         self.down1 = DownDS(64, 128, kernels_per_layer=self.kernels_per_layer)
         self.down2 = DownDS(128, 256, kernels_per_layer=self.kernels_per_layer)
         self.down3 = DownDS(256, 512, kernels_per_layer=self.kernels_per_layer)
+        factor = 2 if self.bilinear else 1
         self.down4 = DownDS(512, 1024 // factor, kernels_per_layer=self.kernels_per_layer)
         # (CBAM) are commonly applied to the output of the final convolutional block in an encoder is to emphasize the
         # most discriminative features in the input image that are relevant to the task being solved.
@@ -25,11 +29,11 @@ class SmaAt_UNet_pre(nn.Module):
 
         # Fully connected layer
         if self.bilinear:
-            self.fc = nn.Linear(512, num_classes)
+            self.fc = nn.Linear(512, self.num_classes)
         else:
-            self.fc = nn.Linear(1024, num_classes)
+            self.fc = nn.Linear(1024, self.num_classes)
 
-    def forward(self, x):
+    def forward(self, x: Tensor) -> Tensor:
         x1 = self.inc(x)
         x2 = self.down1(x1)
         x3 = self.down2(x2)
