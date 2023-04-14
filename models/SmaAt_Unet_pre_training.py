51ba8a8523500b9e0d441719e9823fdcf7b468ee
Tamara Sessink <t.r.sessink@students.uu.nl>
2023-04-14 14:52:55 +0200

    update project

--- models/SmaAt_Unet_pre_training.py
+++ models/SmaAt_Unet_pre_training.py
@@ -3,61 +3,45 @@ from models.unet_parts import OutConv
 from models.unet_parts_depthwise_separable import DoubleConvDS, UpDS, DownDS
 from models.layers import CBAM
 
-#TODO: add copyright
+
+# TODO: add copyright
 
 class SmaAt_UNet_pre(nn.Module):
     def __init__(self, num_classes: 12):
         super(SmaAt_UNet_pre, self).__init__()
-        self.n_channels = 3
-        # self.num_classes = 12
-        kernels_per_layer = 2
+        self.num_channels = 3
+        self.kernels_per_layer = 2
         self.bilinear = True
-        reduction_ratio = 16
+        self.reduction_ratio = 16
 
-        self.inc = DoubleConvDS(self.n_channels, 64, kernels_per_layer=kernels_per_layer)
-        self.cbam1 = CBAM(64, reduction_ratio=reduction_ratio)
-        self.down1 = DownDS(64, 128, kernels_per_layer=kernels_per_layer)
-        self.cbam2 = CBAM(128, reduction_ratio=reduction_ratio)
-        self.down2 = DownDS(128, 256, kernels_per_layer=kernels_per_layer)
-        self.cbam3 = CBAM(256, reduction_ratio=reduction_ratio)
-        self.down3 = DownDS(256, 512, kernels_per_layer=kernels_per_layer)
-        self.cbam4 = CBAM(512, reduction_ratio=reduction_ratio)
-        factor = 2 if self.bilinear else 1
-        self.down4 = DownDS(512, 1024 // factor, kernels_per_layer=kernels_per_layer)
-        self.cbam5 = CBAM(1024 // factor, reduction_ratio=reduction_ratio)
-        self.up1 = UpDS(1024, 512 // factor, self.bilinear, kernels_per_layer=kernels_per_layer)
-        self.up2 = UpDS(512, 256 // factor, self.bilinear, kernels_per_layer=kernels_per_layer)
-        self.up3 = UpDS(256, 128 // factor, self.bilinear, kernels_per_layer=kernels_per_layer)
-        self.up4 = UpDS(128, 64, self.bilinear, kernels_per_layer=kernels_per_layer)
+        self.inc = DoubleConvDS(self.n_channels, 64, kernels_per_layer=self.kernels_per_layer)
+        self.down1 = DownDS(64, 128, kernels_per_layer=self.kernels_per_layer)
+        self.down2 = DownDS(128, 256, kernels_per_layer=self.kernels_per_layer)
+        self.down3 = DownDS(256, 512, kernels_per_layer=self.kernels_per_layer)
+        self.down4 = DownDS(512, 1024 // factor, kernels_per_layer=self.kernels_per_layer)
+        # (CBAM) are commonly applied to the output of the final convolutional block in an encoder is to emphasize the
+        # most discriminative features in the input image that are relevant to the task being solved.
+        self.cbam5 = CBAM(1024 // factor, reduction_ratio=self.reduction_ratio)
 
-        # self.fc = nn.Linear(512 * block.expansion, num_classes)
-        # self.pooling = nn.AdaptiveAvgPool2d(output_size=(1, 1))
-        self.outc = OutConv(64, num_classes)
+        # Fully connected layer
+        if self.bilinear:
+            self.fc = nn.Linear(512, num_classes)
+        else:
+            self.fc = nn.Linear(1024, num_classes)
 
     def forward(self, x):
         x1 = self.inc(x)
-        x1Att = self.cbam1(x1)
         x2 = self.down1(x1)
-        x2Att = self.cbam2(x2)
         x3 = self.down2(x2)
-        x3Att = self.cbam3(x3)
         x4 = self.down3(x3)
-        x4Att = self.cbam4(x4)
         x5 = self.down4(x4)
-        x5Att = self.cbam5(x5)
-        x = self.up1(x5Att, x4Att)
-        x = self.up2(x, x3Att)
-        x = self.up3(x, x2Att)
-        x = self.up4(x, x1Att)
 
-        # apply global average pooling to reduce the spatial dimensions to 1x1
-        x = nn.functional.adaptive_avg_pool2d(x, (1, 1))
+        x5Att = self.cbam5(x5)
 
         # reshape to a 1D vector
-        x = x.view(x.size(0), -1)
+        x = x5Att.view(x5Att.size(0), -1)
 
-        # apply a linear layer to output a fixed-dimensional feature vector
-        # encoder to project the output feature map to a lower-dimensional space, which is then used as the feature representation for the MoCo model
+        # fully connected layer
         x = self.fc(x)
 
         # return the feature vector
